b079de50b8ba48de402807f5c5c20f3da024e20d
Jozsef palosi <jpalosi@mbp-de-jozsef.home>
2023-03-13 14:52:24 +0100

    Change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there Jozsi! Yo
+          Szia Jozsi!
         </p>
         <a
           className="App-link"
